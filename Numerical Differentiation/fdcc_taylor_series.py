6e386d74d09f8967e2a16284fecbd9fa16d0eb1b
Streetlight321 <98481263+Streetlight321@users.noreply.github.com>
2022-12-28 00:18:35 -0500

    Update fdcc_taylor_series.py

--- Numerical Differentiation/fdcc_taylor_series.py
+++ Numerical Differentiation/fdcc_taylor_series.py
@@ -61,50 +61,85 @@ class fdcc:
     [1] Taylor, Cameron R. (2016). Finite Difference Coefficients Calculator. [Online]. Available at: https://web.media.mit.edu/~crtaylor/calculator.html
     """
     def __init__(self, s, k):
+        """
+        Initializes the FiniteDifference object.
         
+        Args:
+            s (array-like): A 1D array of n stencil points (i.e., points relative to x at which the derivative of f is to be approximated).
+            k (int): The order of the derivative to be approximated (must be at least 1 and strictly less than n).
+        """
+        # Check input values of s and k for validity
         s = self._check_input(s,k)
-        
+
+        # Define symbols x and h, and function f with real inputs
         x, h = symbols('x,h',real=True)
         f = Function('f',real=True)
-        
+
+        # Define xs as a list of si*h for each element si in s
         xs = [si*h for si in s]
+
+        # Define fs as a list of f(x + xsi) for each element xsi in xs
         fs = [f(x + xsi) for xsi in xs]
 
         n = len(s)
+        # Set terms to n + 2 if the median value of s is 0, or to n + 1 otherwise
         if median(s)==0:
             terms = n+2
         else:
             terms = n+1
+            
+        # Initialize A as an (terms x n) matrix of ones
         A = ones(terms,n)
+
+        # Iterate over rows and columns of A, and set each element to s[j]**i / factorial(i)
         for j in range(n):
             for i in range(1,terms):
                 A[i,j] = s[j]**i / factorial(i)
+
+        # Initialize b as a matrix with n elements, with the k-th element being 1 and all the others being 0
         b = Matrix([0 if i!=k else 1 for i in range(n)])
+
+        # Initialize c as the solution to the linear system (A[:n,:n],b)
         c = Matrix( linsolve((A[:n,:n],b)).args[0] )
 
+        # Set terms to n + 2 if the median value of s is 0, or to n + 1 otherwise
         if median(s)==0:
             terms = n+2
         else:
             terms = n+1
+
+        # Initialize T as an (terms x n) matrix of ones
         T = ones(terms,n)
+        
+        # Iterate over rows and columns of T, and set each element to either f(x) (if i is 0) or f(x).diff(x,i) * (xs[j])**i / factorial(i) (if i is not 0)
         for j in range(n):
             for i in range(terms):
                 if i==0:
                     T[i,j] = f(x)
                 else:
                     T[i,j] = f(x).diff(x,i) * (xs[j])**i / factorial(i)
+
+        # Define cons as the difference between f(x).diff(x,k) and the sum of the products of T and c divided by h**k
         cons = f(x).diff(x,k) - sum(T*c/h**k)
-        
+
+         # Set the finite_difference attribute to the dot product of c and fs divided by h**k, using the cancel method to simplify the result
         self.finite_difference = ( c.dot(fs)/h**k ).cancel()
+        # Set the finite_diff_weights attribute to a list of the elements of c
         self.finite_diff_weights = list(c)
+        # Set the principal_error_term attribute to the value of cons
         self.principal_error_term = cons
         
     def __repr__(self):
+        # theoretically returns a string representation of the FiniteDifference object.
         attrs = ['finite_difference', 'finite_diff_weights', 'principal_error_term']
+        # List of attributes to be included in the representation
         m = max(map(len, attrs)) + 1
+        # Find the maximum length of the attribute names
         return '\n'.join([a.rjust(m) + ': ' + repr(getattr(self,a)) for a in attrs])
+        # Return a string with the attribute names and values, each on a separate line
     
     @staticmethod
+
     def _check_input(s,k):
         s = asarray(s)
         # s must be 1d
